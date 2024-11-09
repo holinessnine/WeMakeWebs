07309b2b39b9671cf187d5c0e156629360f34243
holinessnine <seg3981@gmail.com>
2024-11-09 21:25:51 +0900

    되나?

---
+++ wemakewebs/components/Footer.tsx
@@ -0,0 +1,13 @@
+// components/Footer.tsx
+
+const Footer = () => {
+  return (
+    <footer className="bg-neutral text-neutral-content p-4 mt-10 flex justify-between items-center">
+      <div className="ml-4">
+      </div>
+      <p className="flex-1 text-center">&copy; 2024 Viba Blog. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ wemakewebs/components/ThemeSwitcher.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
+
+const ThemeSwitcher = () => {
+  useEffect(() => {
+    themeChange(false);
+  }, []);
+
+  return (
+    <div className="dropdown dropdown-top">
+      <div tabIndex={0} className="btn m-1">
+        Theme
+        <svg
+          width="12px"
+          height="12px"
+          className="inline-block h-2 w-2 fill-current opacity-60 ml-1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 2048 2048"
+        >
+          <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="dropdown-content bg-base-300 rounded-box w-52 p-2 shadow"
+      >
+        <li>
+          <button
+            data-set-theme="synthwave"
+            className="btn btn-sm btn-block btn-ghost justify-start text-base-content"
+          >
+            Synthwave
+          </button>
+        </li>
+        <li>
+          <button
+            data-set-theme="corporate"
+            className="btn btn-sm btn-block btn-ghost justify-start text-base-content"
+          >
+            Corporate
+          </button>
+        </li>
+        <li>
+          <button
+            data-set-theme="business"
+            className="btn btn-sm btn-block btn-ghost justify-start text-base-content"
+          >
+            Business
+          </button>
+        </li>
+        <li>
+          <button
+            data-set-theme="winter"
+            className="btn btn-sm btn-block btn-ghost justify-start text-base-content"
+          >
+            Winter
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ThemeSwitcher;
+
